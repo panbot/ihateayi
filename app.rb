fbc636dc2c412f71baa0d6357767e2c02bec5dff
Botao Pan <panbot@ubuntu-cf.(none)>
2011-12-03 21:46:19 +0800

    first version

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require 'rubygems'
 require 'sinatra'
-#require 'mongo'
+require 'mongo'
 require 'json'
+require 'bson'
 
-#db = Mongo::Connection.new.db('ihateayi')
-#deedCollection = db['deeds']
+services = JSON.parse(ENV['VMC_SERVICES'])
+opt = services[0]['options']
+db = Mongo::Connection.new(opt['host'], opt['port']).db(opt['db'])
+db.authenticate(opt['username'], opt['password'])
+deedCollection = db['deeds']
 
+get '/favicon.ico' do
+	send_file 'favicon.ico'
+end
 
 get '/' do
 	send_file 'views/index.html'
@@ -13,17 +20,39 @@ end
 
 get '/whatayidid' do
 	result = []
-	deedCollection.find().each do |deed|
-		result.push({
-			'content' => deed.content
-		})
+	deedCollection.find.each do |deed|
+		result.push(deed)
 	end
-	JSON.stringify(result)
+	JSON.generate(result)
+end
+
+post '/agree' do
+	deedCollection.update(
+		{ '_id' => BSON::ObjectId(params['id']) },
+		{ '$inc' => { 'agreements' => 1 } } )
+	'success'
+end
+
+post '/disagree' do
+	deedCollection.update(
+		{ '_id' => BSON::ObjectId(params['id']) },
+		{ '$inc' => { 'disagreements' => 1 } } )
+	'success'
 end
 
 post '/tell' do
 	deed = {
-		'content' => params[:content]
+		'what' => params['what'],
+		'who' => params['who'],
+		'when' => Time.now,
+		'agreements' => 0,
+		'disagreements' => 0,
 	}
 	deedCollection.insert(deed)
+	'success'
 end
+
+get '/purge' do
+	deedCollection.remove
+end
+
